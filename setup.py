7228577421282cc8494884e6f2c26a64a42bdc49
Charanjit Singh <ckhabra@gmail.com>
2020-11-16 21:57:31 +0530

    feat: tis but a nix flake

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='Fireword',
+      version='1.0',
+      description='Sizzling hot passwords',
+      author='Charanjit Singh',
+      author_email='ckhabra@gmail.com',
+      url='https://channikhabra.com/fireword',
+      scripts=['fireword'],
+      )
